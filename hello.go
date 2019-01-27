2bb81ad46e8511fe76d0bd4de803955bdd1bedf4
brunoflament <bruno.flament.bf@gmail.com>
2019-01-27 20:49:53 +0100

    add revese

--- hello.go
+++ hello.go
@@ -1,7 +1,10 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/brunoflament/stringutil"
+)
 
 func main() {
-	fmt.Println("Hello, world.")
+	fmt.Println(stringutil.Reverse("Hello, world."))
 }
